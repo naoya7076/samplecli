f23c882324bace37be7cd6dfd9496c1e8800258b
naoya7076 <naoya7076@gmail.com>
2020-10-18 10:06:41 +0900

    :tada: マクロを使う形式に変換

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,30 @@
-extern crate clap;
-use clap::{App, Arg};
+use clap::Clap;
+/**
+ * [#derive]アトリビュートを用いることで型に対して特定のトレイトの標準的な実装を提供する機能があります。
+ * より複雑なことを行わせたい場合には、同名のトレイトを手動で実装することも可能です。
+ * deriveマクロは#[derive(MACRO_NAME)]という形式でstructやenumへの修飾を可能にするマクロ
+ * DOC:https://docs.rs/clap/3.0.0-beta.2/clap/index.html#using-derive-macros
+ **/
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Shimmy",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
 
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 fn main() {
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("Shimmy")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("formula_file")
-                .about("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-                .about("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
-    // マッチしたパターンに応じてコードを実行する
-    match matches.value_of("formula_file") {
+    //https://docs.rs/clap/3.0.0-beta.2/clap/trait.Clap.html#method.parse
+    let opts = Opts::parse();
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified"),
+        None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose)
+    println!("Is verbosity specified?: {}", opts.verbose)
 }
