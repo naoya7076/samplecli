503eee1d650776c198f837af5bb4d4403f3d209f
naoya7076 <naoya7076@gmail.com>
2020-10-19 22:43:34 +0900

    計算処理の作成

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,45 @@ impl RpnCalculator {
     }
 
     pub fn eval(&self, formula: &str) -> i32 {
-        0
+        // あとでpopするからrev()している
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        // どうしてここでポインタを渡しているのか -> 借用
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            // 数値に変換できるかどうかを試す
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x)
+            } else {
+                // expectはunwrapとほぼ同じだが、パニック時に指定したメッセージを出力できる
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res)
+            }
+
+            // `-v`オプションが指定されている場合は、この時点でトークンとスタックの状態を出力
+            // 無名フィールドはself.0で取得できる0は何番目のフィールドかを表す
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
     }
 }
