0eb0ebf6460d56e13d232a10928b77ad190c63b3
naoya7076 <naoya7076@gmail.com>
2020-10-22 21:43:52 +0900

    カスタムエラー型の実装

--- src/bin/err_panic.rs
+++ src/bin/err_panic.rs
@@ -1,15 +1,25 @@
-fn get_int_from_file() -> i32 {
+enum MyError {
+  Io(std::io::Error),
+  Num(std::num::ParseIntError),
+}
+fn get_int_from_file() -> Result<i32, MyError> {
   let path = "number.txt";
-
-  let num_str = std::fs::read_to_string(path).expect("failed to open the file");
-  let ret = num_str
+  // map_errはOKの場合はそのまま値を帰す、Errのときは処理を適用させる
+  // ?は直前の結果のResultの方がOk(t)であればtを返してError(e)なら早期リターンして関数を終了する
+  let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
+  num_str
     .trim()
     .parse::<i32>()
-    .expect("failed to parse string to number");
-
-  ret * 2
+    .map(|t| t * 2)
+    .map_err(|e| MyError::Num(e))
 }
 
 fn main() {
-  println!("{}", get_int_from_file());
+  match get_int_from_file() {
+    Ok(x) => println!("{}", x),
+    Err(e) => match e {
+      MyError::Io(cause) => println!("I/O Error: {}", cause),
+      MyError::Num(cause) => println!("Parse Error: {}", cause),
+    },
+  }
 }
