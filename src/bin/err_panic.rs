f86b53958347fb69d425117bd35f2e3b01053781
naoya7076 <naoya7076@gmail.com>
2020-10-23 22:29:42 +0900

    エラーハンドリングにanyhowを使うようにした

--- src/bin/err_panic.rs
+++ src/bin/err_panic.rs
@@ -1,32 +1,21 @@
-enum MyError {
-  Io(std::io::Error),
-  Num(std::num::ParseIntError),
-}
+use anyhow::{Context, Result};
 
-use std::fmt;
-impl fmt::Display for MyError {
-  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    match self {
-      MyError::Io(cause) => write!(f, "I/O Error:{}", cause),
-      MyError::Num(cause) => write!(f, "Parse Error:{}", cause),
-    }
-  }
-}
-fn get_int_from_file() -> Result<i32, MyError> {
+fn get_int_from_file() -> Result<i32> {
   let path = "number.txt";
   // map_errはOKの場合はそのまま値を帰す、Errのときは処理を適用させる
   // ?は直前の結果のResultの方がOk(t)であればtを返してError(e)なら早期リターンして関数を終了する
-  let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
+  let num_str = std::fs::read_to_string(path)
+    .with_context(|| format!("failed to read string from {}", path))?;
   num_str
     .trim()
     .parse::<i32>()
     .map(|t| t * 2)
-    .map_err(|e| MyError::Num(e))
+    .context("failed from parse string")
 }
 
 fn main() {
   match get_int_from_file() {
     Ok(x) => println!("{}", x),
-    Err(e) => println!("{}", e),
+    Err(e) => println!("{:#?}", e),
   }
 }
