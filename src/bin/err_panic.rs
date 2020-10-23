0afa4f28a116c49c134aa1f8522df20cfd229dd3
naoya7076 <naoya7076@gmail.com>
2020-10-23 22:46:53 +0900

    bailとensureの追加

--- src/bin/err_panic.rs
+++ src/bin/err_panic.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::{bail, ensure, Context, Result};
 
 fn get_int_from_file() -> Result<i32> {
   let path = "number.txt";
@@ -6,6 +6,11 @@ fn get_int_from_file() -> Result<i32> {
   // ?は直前の結果のResultの方がOk(t)であればtを返してError(e)なら早期リターンして関数を終了する
   let num_str = std::fs::read_to_string(path)
     .with_context(|| format!("failed to read string from {}", path))?;
+  if num_str.len() >= 10 {
+    bail!("it may be too large number");
+  }
+  // ensureが効かないだと、、、
+  ensure!(num_str.starts_with("1"), "first digit is not 1");
   num_str
     .trim()
     .parse::<i32>()
