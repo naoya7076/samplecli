1f1d5872b296b7fad4101811b7a6e1ce35d3cc08
naoya7076 <naoya7076@gmail.com>
2020-10-23 22:30:01 +0900

    add some file

--- src/bin/async_await.rs
+++ src/bin/async_await.rs
@@ -6,11 +6,11 @@ async fn async_add(left: i32, right: i32) -> i32 {
 
 async fn something_great_async_function() -> i32 {
   let ans = async_add(2, 3).await; //この時点で5という値を取り出せる
-                                   // some process
   println!("{}", ans);
   ans
 }
 
 fn main() {
-  executor::block_on(something_great_async_function())
+  // これがランタイムの起動ポイント
+  executor::block_on(something_great_async_function());
 }
---
+++ src/bin/lifetime.rs
@@ -0,0 +1,4 @@
+//NOTE:わからんかった！！
+async fn some_great_function(arg: &i32) -> i32 {
+  *arg
+}
